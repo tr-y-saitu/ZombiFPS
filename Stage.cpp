3f88aa1d560c5df069098bbe6d2c651dc12ed764
izabera777 <150010641+izabera777@users.noreply.github.com>
2024-07-20 23:53:31 +0900

    ステージクラスを追加

---
+++ Stage.cpp
@@ -0,0 +1,26 @@
+#include "Stage.h"
+#include "Player.h"
+
+
+Stage::Stage()
+{
+}
+
+Stage::~Stage()
+{
+}
+
+void Stage::Initialize()
+{
+
+}
+
+void Stage::Draw()
+{
+
+}
+
+
+void Stage::IsHitCollision(Player& player, const VECTOR& checkPosition, const VECTOR& moveVector)
+{
+}
