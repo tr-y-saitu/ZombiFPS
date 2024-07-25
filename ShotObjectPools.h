9217f6fcebaf745bee5d61b05b9f6546d95683b2
izabera777 <150010641+izabera777@users.noreply.github.com>
2024-07-25 14:56:47 +0900

    ショットのオブジェクトプールを作成

---
+++ ShotObjectPools.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "Common.h"
+
+class Shot;
+
+/// <summary>
+/// 弾丸のオブジェクトプール
+/// </summary>
+class ShotObjectPools
+{
+public:
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    ShotObjectPools();
+
+    /// <summary>
+    /// デストラクタ
+    /// </summary>
+    ~ShotObjectPools();
+
+
+
+private:
+
+};
+
