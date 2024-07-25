6a8c9491559522efb412b3bc841383c95a84f0b4
izabera777 <150010641+izabera777@users.noreply.github.com>
2024-07-25 14:56:47 +0900

    ショットのオブジェクトプールを作成

---
+++ ShotObjectPools.cpp
@@ -0,0 +1,15 @@
+﻿#include "ShotObjectPools.h"
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+ShotObjectPools::ShotObjectPools()
+{
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+ShotObjectPools::~ShotObjectPools()
+{
+}
