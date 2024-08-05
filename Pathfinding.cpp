8b9f70e2a0ab1ba3bff8a56c912e758dd7500be0
izabera777 <150010641+izabera777@users.noreply.github.com>
2024-08-05 14:38:11 +0900

    線形探索クラスを作成

--- Pathfinding.cpp
+++ Pathfinding.cpp
@@ -24,9 +24,37 @@ void Pathfinding::Initialize()
 }
 
 /// <summary>
+/// すべての部屋リストを作成
+/// </summary>
+void Pathfinding::CreateAllRooms()
+{
+    
+}
+
+/// <summary>
 /// 更新
 /// </summary>
 void Pathfinding::Update()
 {
 
+}
+
+
+/// <summary>
+/// エネミーがプレイヤーへどの部屋を経由したら最短か計算し、次の部屋番号を返す
+/// </summary>
+/// <param name="plyaerRoom">プレイヤーのいる部屋</param>
+/// <param name="enemyPosition">エネミーの現在の座標</param>
+/// <returns>エネミーが行くべき部屋</returns>
+Pathfinding::Room Pathfinding::FindRoomPathToPlayer(Room plyaerRoom, VECTOR enemyPosition)
+{
+    // 線形探索を行った結果どの部屋に行けばよいか
+    Room targetRoom;
+
+    // エネミーがどの部屋にいるかを調べる
+
+    // プレイヤーの部屋からエネミーがどの部屋を経由すればよいか検索
+
+    // エネミーが行くべき部屋番号を返す
+    return targetRoom;
 }
--- Pathfinding.h
+++ Pathfinding.h
@@ -8,6 +8,37 @@ class Pathfinding
 {
 public:
     /// <summary>
+    /// 部屋番号
+    /// </summary>
+    enum RoomNumber
+    {
+        West1,      // 西の部屋１
+        West2,      // 西の部屋２
+        West3,      // 西の部屋３
+        West4,      // 西の部屋４
+        Center1,    // 中央の部屋１
+        Center2,    // 中央の部屋２
+        Center3,    // 中央の部屋３
+        Center4,    // 中央の部屋４
+        East1,      // 東の部屋１
+        East2,      // 東の部屋２
+        East3,      // 東の部屋３
+        East4,      // 東の部屋４
+    };
+
+    /// <summary>
+    /// 部屋の情報
+    /// </summary>
+    struct Room
+    {
+        RoomNumber  roomNumber;         // 部屋番号
+        VECTOR      centerPosition;     // 部屋の中心座標（これをエネミーは経由する）
+        float       width;              // 部屋の幅
+        float       depth;              // 部屋の奥行
+        list<Room*> adjacencyRoom;      // 隣接し、現在の部屋から行くことのできる部屋リスト
+    };
+
+    /// <summary>
     /// コンストラクタ
     /// </summary>
     Pathfinding();
@@ -23,11 +54,26 @@ public:
     void Initialize();
 
     /// <summary>
+    /// すべての部屋リストを作成
+    /// </summary>
+    void CreateAllRooms();
+
+    /// <summary>
     /// 更新
     /// </summary>
     void Update();
 
+    /// <summary>
+    /// エネミーがプレイヤーへどの部屋を経由したら最短か計算し、次の部屋番号を返す
+    /// </summary>
+    /// <param name="plyaerRoom">プレイヤーのいる部屋</param>
+    /// <param name="enemyPosition">エネミーの現在の座標</param>
+    /// <returns>エネミーが行くべき部屋</returns>
+    Room FindRoomPathToPlayer(Room plyaerRoom, VECTOR enemyPosition);
+
 private:
+
+    list<Room*>     roomList;       // マップに存在するすべての部屋のリスト
 
 };
 
