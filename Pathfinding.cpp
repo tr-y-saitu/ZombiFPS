95ee06e72baad6ce612465cd8bdd113cd415e2f4
izabera777 <150010641+izabera777@users.noreply.github.com>
2024-08-06 12:05:38 +0900

    部屋の中心座標を設定し描画

--- Pathfinding.cpp
+++ Pathfinding.cpp
@@ -55,18 +55,18 @@ void Pathfinding::CreateRooms()
 void Pathfinding::InitializeRoomsData()
 {
     // 部屋のデータ   // 部屋番号     // 中心座標             // 幅   // 奥行        // 画像データ
-    initRoomData[0]  = { West1,     VGet(-10.0f, 4.5f, 58.0f)      ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::West1ImageData) };
-    initRoomData [1] = { West2,     VGet( -10.0f, 4.5f, -5.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::West2ImageData) };
-    initRoomData [2] = { West3,     VGet(      0, 4.5f,  0.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::West3ImageData) };
-    initRoomData [3] = { West4,     VGet(      0, 4.5f,  0.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::West4ImageData) };
-    initRoomData [4] = { Center1,   VGet(      0, 4.5f,  0.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::Center1ImageData) };
-    initRoomData [5] = { Center2,   VGet(      0, 4.5f,  0.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::Center2ImageData) };
-    initRoomData [6] = { Center3,   VGet(      0, 4.5f,  0.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::Center3ImageData) };
-    initRoomData [7] = { Center4,   VGet(      0, 4.5f,  0.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::Center4ImageData) };
-    initRoomData [8] = { East1,     VGet(      0, 4.5f,  0.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::East1ImageData) };
-    initRoomData [9] = { East2,     VGet(      0, 4.5f,  0.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::East2ImageData) };
-    initRoomData[10] = { East3,     VGet(      0, 4.5f,  0.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::East3ImageData) };
-    initRoomData[11] = { East4,     VGet(      0, 4.5f,  0.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::East4ImageData) };
+    initRoomData[0]  = { West1,     VGet(  -45.0f,  4.5f,  45.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::West1ImageData) };
+    initRoomData [1] = { West2,     VGet(  -45.0f,  4.5f,  18.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::West2ImageData) };
+    initRoomData [2] = { West3,     VGet(  -45.0f,  4.5f,  -5.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::West3ImageData) };
+    initRoomData [3] = { West4,     VGet(  -48.0f,  4.5f, -33.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::West4ImageData) };
+    initRoomData [4] = { Center1,   VGet(  -10.0f,  4.5f,  58.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::Center1ImageData) };
+    initRoomData [5] = { Center2,   VGet(  -10.0f,  4.5f,  -5.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::Center2ImageData) };
+    initRoomData [6] = { Center3,   VGet(  -10.0f,  4.5f, -47.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::Center3ImageData) };
+    initRoomData [7] = { Center4,   VGet(  -10.0f,  4.5f, -75.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::Center4ImageData) };
+    initRoomData [8] = { East1,     VGet(   25.0f,  4.5f,  25.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::East1ImageData) };
+    initRoomData [9] = { East2,     VGet(   45.0f,  4.5f,  -5.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::East2ImageData) };
+    initRoomData[10] = { East3,     VGet(   15.0f,  4.5f, -35.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::East3ImageData) };
+    initRoomData[11] = { East4,     VGet(   50.0f,  4.5f, -35.0f)     ,1      ,1   ,imageDataManager->GetImageHandle(ImageDataManager::East4ImageData) };
 }
 
 /// <summary>
