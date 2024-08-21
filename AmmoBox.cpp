739e1b43aa2cca33fb3736314c3a4d1fcfb931f7
izabera777 <150010641+izabera777@users.noreply.github.com>
2024-08-21 18:16:18 +0900

    弾薬補充箱素材を追加

---
+++ AmmoBox.cpp
@@ -0,0 +1,76 @@
+﻿#include "AmmoBox.h"
+#include "ModelDataManager.h"
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+AmmoBox::AmmoBox()
+    : position      (InitializePosition)
+{
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+AmmoBox::~AmmoBox()
+{
+}
+
+/// <summary>
+/// 初期化
+/// </summary>
+void AmmoBox::Initialize()
+{
+    // 座標設定
+    position = InitializePosition;
+
+    // 当たり判定用情報
+    collisionData.tag               = ObjectTag::AmmoBox;
+    collisionData.centerPosition    = InitializePosition;
+    collisionData.radius            = CollisionRadius;
+    collisionData.interactRadius    = CollisionInteractRadius;
+
+    // モデルデータマネージャーのインスタンスをもらう
+    modelDataManager = ModelDataManager::GetInstance();
+
+    // モデルデータをもらう
+    modelHandle = modelDataManager->GetDuplicatesModelHandle(ModelDataManager::AmmoBox);
+
+    // スケールを設定
+    MV1SetScale(modelHandle, InitializeScale);
+}
+
+/// <summary>
+/// 更新
+/// </summary>
+void AmmoBox::Update()
+{
+    // モデルの座標設定
+    MV1SetPosition(modelHandle, position);
+}
+
+/// <summary>
+/// 描画
+/// </summary>
+void AmmoBox::Draw()
+{
+    // モデルの描画
+    MV1DrawModel(modelHandle);
+
+    // デバッグ当たり判定の描画
+    DrawSphere3D(collisionData.centerPosition, collisionData.radius,
+        DebugSphereDivision, DebugPolygonColorBlue, DebugPolygonColorBlue, false);
+
+    // デバッグシャッターにアクセルできる半径を描画
+    DrawSphere3D(collisionData.centerPosition, collisionData.interactRadius,
+        DebugSphereDivision, DebugPolygonColorRed, DebugPolygonColorRed, false);
+}
+
+/// <summary>
+/// オブジェクトと接触した時の処理
+/// </summary>
+/// <param name="hitObjectData">接触したオブジェクトの情報</param>
+void AmmoBox::OnHit(CollisionData hitObjectData)
+{
+    // 処理なし
+}
---
+++ AmmoBox.h
@@ -0,0 +1,66 @@
+﻿#pragma once
+#include "Common.h"
+#include "CollisionData.h"
+
+class ModelDataManager;
+
+/// <summary>
+/// 弾薬補充箱
+/// </summary>
+class AmmoBox
+{
+public:
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    AmmoBox();
+
+    /// <summary>
+    /// デストラクタ
+    /// </summary>
+    ~AmmoBox();
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    void Initialize();
+
+    /// <summary>
+    /// 更新
+    /// </summary>
+    void Update();
+
+    /// <summary>
+    /// 描画
+    /// </summary>
+    void Draw();
+
+private:
+    /// <summary>
+    /// オブジェクトと接触した時の処理
+    /// </summary>
+    /// <param name="hitObjectData">接触したオブジェクトの情報</param>
+    void OnHit(CollisionData hitObjectData);
+
+
+    //---------------------------------------------------------------------------------//
+    //                                      定数                                       //
+    //---------------------------------------------------------------------------------//
+    static constexpr VECTOR InitializePosition      = { 2,1.3f,6 };         // 初期座標
+    static constexpr VECTOR InitializeScale         = { 0.3f,0.3f,0.3f };   // 初期スケール
+    static constexpr float  CollisionRadius         = 5;                    // 当たり判定半径
+    static constexpr float  CollisionInteractRadius = 10;                   // インタラクトできる判定半径
+
+    //---------------------------------------------------------------------------------//
+    //                                      変数                                       //
+    //---------------------------------------------------------------------------------//
+    // 管理クラス
+    ModelDataManager*   modelDataManager;   // モデルデータ管理クラス
+
+    // ステータス
+    CollisionData       collisionData;      // 当たり判定用情報
+    VECTOR              position;           // 座標
+    int                 modelHandle;        // モデルハンドル
+};
+
+
