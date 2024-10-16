6210b7570ba80d38cdc2f311acb65b89bc3155c2
izabera777 <150010641+izabera777@users.noreply.github.com>
2024-10-16 15:58:37 +0900

    jsonデータを適用できるように修正

---
+++ MapData.cpp
@@ -0,0 +1,16 @@
+﻿// 線形探索用マップデータ //
+
+/*
+  iostream:実行画面上に文字を表示したり文字を入力するのに必要
+  json.hpp:jsonの作成読み込みを楽にしてくれる
+  fstream:ファイルの読み込みや書き込みに必要
+*/
+
+#include "Common.h"
+#include <iostream>
+#include "nlohmann/json.hpp"
+#include <fstream>
+#include "Pathfinding.h"
+
+//書き込むjsonファイルの内容を書く
+
